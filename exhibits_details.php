58ec4ecbf1156ebdaaea81e8b430366cc56a1bc9
Mugdho <saha2171515@stud.kuet.ac.bd>
2023-09-30 17:54:01 +0600

    30/09/2023

--- exhibits_details.php
+++ exhibits_details.php
@@ -8,6 +8,7 @@ $page = 'exhibits';
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Exhibits - Hong Kong Pavilion</title>
+    <link rel='stylesheet' href='https://unpkg.com/xzoom/dist/xzoom.css'>
     <?php include('include/css.php'); ?>
 </head>
 
@@ -26,21 +27,27 @@ $page = 'exhibits';
                 <div class="product-main-area">
                     <div class="row">
                         <div class="col-lg-5 col-md-6 col-12">
-                            <div class="flexslider">
-                                <ul class="slides">
-                                    <li data-thumb="img/flex/1.jpg">
-                                        <img src="img/flex/1.jpg" alt="book" />
-                                    </li>
-                                    <li data-thumb="img/flex/1.jpg">
-                                        <img src="img/flex/1.jpg" alt="book" />
-                                    </li>
-                                    <li data-thumb="img/flex/1.jpg">
-                                        <img src="img/flex/1.jpg" alt="book" />
-                                    </li>
-                                    <li data-thumb="img/flex/1.jpg">
-                                        <img src="img/flex/1.jpg" alt="book" />
-                                    </li>
-                                </ul>
+                            <div class="xzoom-container">
+                                <img class="xzoom" id="xzoom-default"
+                                     src="img/flex/Demo.jpg" width="400" height="400"
+                                     xoriginal="img/flex/Demo.jpg"/>
+                                <div class="xzoom-thumbs">
+                                    <a href="img/flex/Demo.jpg">
+                                        <img class="xzoom-gallery" width="80"
+                                             src="img/flex/Demo.jpg"></a>
+
+                                    <a href="img/flex/Demo.jpg">
+                                        <img class="xzoom-gallery" width="80"
+                                             src="img/flex/Demo.jpg"></a>
+
+                                    <a href="img/flex/Demo.jpg">
+                                        <img class="xzoom-gallery" width="80"
+                                             src="img/flex/Demo.jpg"></a>
+
+                                    <a href="img/flex/Demo.jpg">
+                                        <img class="xzoom-gallery" width="80"
+                                             src="img/flex/Demo.jpg"></a>
+                                </div>
                             </div>
                         </div>
                         <div class="col-lg-7 col-md-6 col-12">
@@ -135,7 +142,6 @@ $page = 'exhibits';
                         <h3>Company Exhibit List</h3>
                     </div>
                     <div class="tab-active-2 owl-carousel owl-loaded owl-drag">
-
                         <!-- single-product-end -->
                         <div class="owl-stage-outer">
                             <div class="owl-stage"
@@ -181,8 +187,165 @@ $page = 'exhibits';
 <!-- footer-area-end -->
 
 
-<!-- all js here -->
 <?php include('include/js.php'); ?>
+<script src='https://code.jquery.com/jquery-2.1.1.js'></script>
+<script src='https://unpkg.com/xzoom/dist/xzoom.min.js'></script>
+<script src='https://hammerjs.github.io/dist/hammer.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/js/foundation.min.js'></script>
+<script>
+    (function ($) {
+        $(document).ready(function () {
+            $('.xzoom, .xzoom-gallery').xzoom({zoomWidth: 400, title: true, tint: '#333', Xoffset: 15});
+            $('.xzoom2, .xzoom-gallery2').xzoom({position: '#xzoom2-id', tint: '#ffa200'});
+            $('.xzoom3, .xzoom-gallery3').xzoom({position: 'lens', lensShape: 'circle', sourceClass: 'xzoom-hidden'});
+            $('.xzoom4, .xzoom-gallery4').xzoom({tint: '#006699', Xoffset: 15});
+            $('.xzoom5, .xzoom-gallery5').xzoom({tint: '#006699', Xoffset: 15});
+
+            //Integration with hammer.js
+            var isTouchSupported = 'ontouchstart' in window;
+
+            if (isTouchSupported) {
+                //If touch device
+                $('.xzoom, .xzoom2, .xzoom3, .xzoom4, .xzoom5').each(function () {
+                    var xzoom = $(this).data('xzoom');
+                    xzoom.eventunbind();
+                });
+
+                $('.xzoom, .xzoom2, .xzoom3').each(function () {
+                    var xzoom = $(this).data('xzoom');
+                    $(this).hammer().on("tap", function (event) {
+                        event.pageX = event.gesture.center.pageX;
+                        event.pageY = event.gesture.center.pageY;
+                        var s = 1, ls;
+
+                        xzoom.eventmove = function (element) {
+                            element.hammer().on('drag', function (event) {
+                                event.pageX = event.gesture.center.pageX;
+                                event.pageY = event.gesture.center.pageY;
+                                xzoom.movezoom(event);
+                                event.gesture.preventDefault();
+                            });
+                        }
+
+                        xzoom.eventleave = function (element) {
+                            element.hammer().on('tap', function (event) {
+                                xzoom.closezoom();
+                            });
+                        }
+                        xzoom.openzoom(event);
+                    });
+                });
+
+                $('.xzoom4').each(function () {
+                    var xzoom = $(this).data('xzoom');
+                    $(this).hammer().on("tap", function (event) {
+                        event.pageX = event.gesture.center.pageX;
+                        event.pageY = event.gesture.center.pageY;
+                        var s = 1, ls;
+
+                        xzoom.eventmove = function (element) {
+                            element.hammer().on('drag', function (event) {
+                                event.pageX = event.gesture.center.pageX;
+                                event.pageY = event.gesture.center.pageY;
+                                xzoom.movezoom(event);
+                                event.gesture.preventDefault();
+                            });
+                        }
+
+                        var counter = 0;
+                        xzoom.eventclick = function (element) {
+                            element.hammer().on('tap', function () {
+                                counter++;
+                                if (counter == 1) setTimeout(openfancy, 300);
+                                event.gesture.preventDefault();
+                            });
+                        }
+
+                        function openfancy() {
+                            if (counter == 2) {
+                                xzoom.closezoom();
+                                $.fancybox.open(xzoom.gallery().cgallery);
+                            } else {
+                                xzoom.closezoom();
+                            }
+                            counter = 0;
+                        }
+
+                        xzoom.openzoom(event);
+                    });
+                });
+
+                $('.xzoom5').each(function () {
+                    var xzoom = $(this).data('xzoom');
+                    $(this).hammer().on("tap", function (event) {
+                        event.pageX = event.gesture.center.pageX;
+                        event.pageY = event.gesture.center.pageY;
+                        var s = 1, ls;
+
+                        xzoom.eventmove = function (element) {
+                            element.hammer().on('drag', function (event) {
+                                event.pageX = event.gesture.center.pageX;
+                                event.pageY = event.gesture.center.pageY;
+                                xzoom.movezoom(event);
+                                event.gesture.preventDefault();
+                            });
+                        }
+
+                        var counter = 0;
+                        xzoom.eventclick = function (element) {
+                            element.hammer().on('tap', function () {
+                                counter++;
+                                if (counter == 1) setTimeout(openmagnific, 300);
+                                event.gesture.preventDefault();
+                            });
+                        }
+
+                        function openmagnific() {
+                            if (counter == 2) {
+                                xzoom.closezoom();
+                                var gallery = xzoom.gallery().cgallery;
+                                var i, images = new Array();
+                                for (i in gallery) {
+                                    images[i] = {src: gallery[i]};
+                                }
+                                $.magnificPopup.open({items: images, type: 'image', gallery: {enabled: true}});
+                            } else {
+                                xzoom.closezoom();
+                            }
+                            counter = 0;
+                        }
+
+                        xzoom.openzoom(event);
+                    });
+                });
+
+            } else {
+                //If not touch device
+
+                //Integration with fancybox plugin
+                $('#xzoom-fancy').bind('click', function (event) {
+                    var xzoom = $(this).data('xzoom');
+                    xzoom.closezoom();
+                    $.fancybox.open(xzoom.gallery().cgallery, {padding: 0, helpers: {overlay: {locked: false}}});
+                    event.preventDefault();
+                });
+
+                //Integration with magnific popup plugin
+                $('#xzoom-magnific').bind('click', function (event) {
+                    var xzoom = $(this).data('xzoom');
+                    xzoom.closezoom();
+                    var gallery = xzoom.gallery().cgallery;
+                    var i, images = new Array();
+                    for (i in gallery) {
+                        images[i] = {src: gallery[i]};
+                    }
+                    $.magnificPopup.open({items: images, type: 'image', gallery: {enabled: true}});
+                    event.preventDefault();
+                });
+            }
+        });
+    })(jQuery);
+</script>
 
 </body>
 
